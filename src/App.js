291a2c3d6b6956ab27629aafba5e9eb6a984030d
Soubhik Laha <soubhik.laha@brainiuminfotech.com>
2023-06-26 16:16:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    <Helmet>
+      <meta
+        property="og:url"
+        content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+      />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="When Great Minds Don’t Think Alike" />
+      <meta
+        property="og:description"
+        content="How much does culture influence creative thinking?"
+      />
+      <meta
+        property="og:image"
+        content="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
+      />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@nytimesbits" />
+      <meta name="twitter:creator" content="@nickbilton" />
+      <meta
+        property="og:url"
+        content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/"
+      />
+      <meta property="og:title" content="A Twitter for My Sister" />
+      <meta
+        property="og:description"
+        content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling."
+      />
+      <meta
+        property="og:image"
+        content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
+      />
+
+
+    </Helmet>
+    // </div>
   );
 }
 
